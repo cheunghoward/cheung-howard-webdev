88797bf4d99baf3facca257b242250056b8d8817
Howard Cheung <cheunghoward@users.noreply.github.com>
2017-01-17 16:48:59 -0500

    Set up for Azure

--- test/app.js
+++ test/app.js
@@ -4,15 +4,7 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
-
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
-    }
+    var connectionString = process.env.mongo || 'mongodb://127.0.0.1:27017/test';
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
