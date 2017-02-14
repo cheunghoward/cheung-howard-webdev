6834c0a1adc1bf3e7b84a2f8764b06891de0d710
Howard Cheung <cheunghoward@users.noreply.github.com>
2017-02-14 18:51:28 -0500

    Begin angular routing for login, move html files into respective folders

---
+++ public/assignment/config.js
@@ -0,0 +1,13 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .config(configuration);
+
+    function configuration($routeProvider) {
+        $routeProvider
+            .when("/login",{
+                templateUrl: 'user/login.html',
+            });
+
+    }
+})();
